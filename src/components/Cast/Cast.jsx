2fa44cc3d72c70ef7a09b82fbc417503541101ca
VasylDanylevych <ukrainec31@gmail.com>
2023-04-01 18:40:11 +0300

    12312

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,5 +2,6 @@ import { useParams } from 'react-router-dom';
 
 export const Cast = () => {
   const { movieId } = useParams();
+
   return <div>Movie cast: {movieId}</div>;
 };
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,5 +2,6 @@ import { useParams } from 'react-router-dom';
 
 export const Reviews = () => {
   const { movieId } = useParams();
+
   return <div>Movie reviews: {movieId}</div>;
 };
