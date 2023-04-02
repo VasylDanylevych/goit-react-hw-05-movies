d23db41d9bf6d535598005fd368dc79bec96fe4e
VasylDanylevych <ukrainec31@gmail.com>
2023-04-02 12:27:52 +0300

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 import { FormEl, Input } from './Movies.style';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Movies() {
   const [movie, setMovie] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query');
 
   const handleSubmit = evt => {
     evt.preventDefault();
+    setSearchParams({ query: evt.target.value });
     console.log('movie: ', movie);
+    console.log('query: ', query);
   };
 
   return (
@@ -23,3 +28,4 @@ export default function Movies() {
     </FormEl>
   );
 }
+//
